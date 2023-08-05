34337c5af09c26c15e8e210e7fc9269fb597c28f
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-08-05 15:08:57 -0400

    Add files via upload

--- 01_Arduino_motor_encoder_pos/01_Arduino_motor_encoder_pos.ino
+++ 01_Arduino_motor_encoder_pos/01_Arduino_motor_encoder_pos.ino
@@ -1,13 +1,23 @@
 #include <PID_v1.h>
 
+// Define math constant for calculus
+// (not sure if it's included in arduino.h)
+
+#define PI 3.1415926535897932384626433832795
+#define HALF_PI 1.5707963267948966192313216916398
+#define TWO_PI 6.283185307179586476925286766559
+#define DEG_TO_RAD 0.017453292519943295769236907684886
+#define RAD_TO_DEG 57.295779513082320876798154814105
 
 // Define encoder pins and interrupt numbers
 // Arduino UNO pins with interrupt : 2, 3.
 const int encoderAPin = 2;
 const int encoderBPin = 3;
 
-// Encoders parameters
+// Encoders parameters --> Phase = 4 for quadrature
 const int encoderResolution = 120;
+const int phase = 4;
+const int countPerRevolution = phase * encoderResolution;
 
 // Define motor control pins
 
@@ -26,8 +36,8 @@ const long serialBaudRate = 115200;
 
 // Variables 
 
-volatile int encoderCount = 0; 
-volatile int lastEncoded = 0;
+volatile long encoderCount = 0; 
+volatile long lastEncoded = 0;
 volatile float measuredAngle = 0.0;
 
 void setup() {
@@ -78,20 +88,25 @@ void updateEncoder(){
   int encoded = (MSB << 1) | LSB;
   int sum = (lastEncoded << 2) | encoded;
 
-/*
+  // TEST TO PRINT SEQUENCE
+  /* 
   Serial.print(MSB);
   Serial.print(", ");
   Serial.println(LSB);
-*/
+  */
 
   if (sum == 0b0001 || sum == 0b0111 || sum == 0b1110 || sum == 0b1000) {
     encoderCount ++;
   } else if (sum == 0b0010 || sum == 0b1011 || sum == 0b1101 || sum == 0b0100) {
     encoderCount --;
   }
-  
-  Serial.println(encoderCount);
+
   lastEncoded = encoded;
+
+  // TEST TO PRINT COUNT
+  /*
+  Serial.println(encoderCount);
+  */
 }
 
 void initEncoder(){
@@ -99,4 +114,8 @@ void initEncoder(){
   int MSB = digitalRead(encoderAPin);
   int LSB = digitalRead(encoderBPin);
   lastEncoded = (MSB << 1) | LSB;
+}
+
+void calculAngle() {
+  measuredAngle = TWO_PI * ((float)encoderCount / countPerRevolution);
 }
