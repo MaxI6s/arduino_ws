9ce11dc5c4adc98b3d59e1cee525953ace5f39d2
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-08-05 15:09:33 -0400

    Update 01_Arduino_motor_encoder_pos.ino

--- 01_Arduino_motor_encoder_pos/01_Arduino_motor_encoder_pos.ino
+++ 01_Arduino_motor_encoder_pos/01_Arduino_motor_encoder_pos.ino
@@ -2,7 +2,6 @@
 
 // Define math constant for calculus
 // (not sure if it's included in arduino.h)
-
 #define PI 3.1415926535897932384626433832795
 #define HALF_PI 1.5707963267948966192313216916398
 #define TWO_PI 6.283185307179586476925286766559
